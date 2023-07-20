1b24eaa0864ce6900b26b77d259f0192ad14c9d2
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-07-20 13:37:11 -0600

    adds tbody and delete srcset from img code

--- client/src/views/avatar.jsx
+++ client/src/views/avatar.jsx
@@ -9,16 +9,18 @@ const Main = (props) => {
             <form onSubmit={props.onSubmitHandler}>
                 <div style={{display: "flex", justifyContent: "center"}}>
                 <table>
-                    <tr>
-                    <td><a href=''><img src="/blue_icon.png" alt="blue" srcset="" /></a></td>
-                    <td><a href=''><img src="/red_icon.png" alt="red" srcset="" /></a></td>
-                    <td><a href=''><img src="/yellow_icon.png" alt="yellow" srcset="" /></a></td>
-                    </tr>
-                    <tr>
-                    <td><a href=''><img src="/green_icon.png" alt="green" /></a></td>
-                    <td><a href=''><img src="/purple_icon.png" alt="purple" /></a></td>
-                    <td><a href=''><img src="/grey_icon.png" alt="grey" srcset="" /></a></td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                        <td><a href=''><img src="/blue_icon.png" alt="blue" /></a></td>
+                        <td><a href=''><img src="/red_icon.png" alt="red" /></a></td>
+                        <td><a href=''><img src="/yellow_icon.png" alt="yellow" /></a></td>
+                        </tr>
+                        <tr>
+                        <td><a href=''><img src="/green_icon.png" alt="green" /></a></td>
+                        <td><a href=''><img src="/purple_icon.png" alt="purple" /></a></td>
+                        <td><a href=''><img src="/grey_icon.png" alt="grey" /></a></td>
+                        </tr>
+                    </tbody>
                 </table>
                 </div>
             </form>
