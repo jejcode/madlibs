74ebeb01f21265313bd3c072e33dd8160da4e9f8
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-07-24 10:19:05 -0600

    fixes routes and modularizes generateRoomCode

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import cors from "cors";
 import express from "express";
 import { Server } from "socket.io";
 import dbConnect from "./mongo/dbConnect.js";
-
+import templateRouter from "./routes/template.routes.js";
+import { generateRoomCode } from "./utils/server-functions.js";
 const app = express();
 
 // Enable CORS for the Express server to accept requests from http://localhost:5173
@@ -11,6 +12,9 @@ app.use(cors({ origin: "http://localhost:5173" }));
 // Parse incoming JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Set up routes
+app.use("/api/templates", templateRouter);
 
 async function serverStart() {
   try {
@@ -38,16 +42,7 @@ async function serverStart() {
       // creates a key:value pair in rooms {roomCode: [array of user names in that room]}
       // and returns the room code to the client for navigation
       socket.on("CREATE_ROOM_REQUEST", (reqName) => {
-        const generateRoomCode = () => {
-          const makeKey = () => {
-            return Math.random().toString(36).substring(2, 8).toUpperCase();
-          };
-          const newKey = makeKey();
-          while (newKey == rooms[newKey]) {
-            newKey = makeKey();
-          }
-          return newKey;
-        };
+        
         const newRoomCode = generateRoomCode();
         rooms[newRoomCode] = [reqName];
         console.log('generated room key:', newRoomCode)
