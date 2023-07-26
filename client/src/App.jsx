e5420ed623fb99a5513db0849bdef0c5dd9025d7
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-07-25 19:23:36 -0600

    fixes module linking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,28 +3,28 @@ import React from "react";
 import RoomView from "./views/RoomView";
 import "bootstrap/dist/css/bootstrap.min.css";
 import RegisterView from "./views/RegisterView";
-import AddMadLibView from "./views/AddMadlibView";
 import Container from "react-bootstrap/Container";
 import DashboardView from "./views/DashboardView";
 import EditMadLibView from "./views/EditMadLibView";
-import {socket, SocketContext} from './contexts/socket'
+import CreateMadLibView from "./views/CreateMadLibView";
+import { socket, SocketContext } from "./contexts/socket";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-  const resetPage = () => {
-    
-  }
+  const resetPage = () => {};
   return (
     <>
-      
       <Container>
         <SocketContext.Provider value={socket}>
           <Router>
             <Routes>
-                <Route path="/" element={<RegisterView />} />
-                <Route path="/dashboard" element={<DashboardView />} />
-                <Route path="/rooms/:roomId" element={<RoomView />} />
-                <Route path="/madlibs/new" element={<AddMadLibView />} />
-                <Route path="/madlibs/:madLibId/edit" element={<EditMadLibView />} />
+              <Route path="/" element={<RegisterView />} />
+              <Route path="/dashboard" element={<DashboardView />} />
+              <Route path="/rooms/:roomId" element={<RoomView />} />
+              <Route path="/madlibs/new" element={<CreateMadLibView />} />
+              <Route
+                path="/madlibs/:madLibId/edit"
+                element={<EditMadLibView />}
+              />
             </Routes>
           </Router>
         </SocketContext.Provider>
