255ec9c77979a76636bd278e906cc4eeaae8b488
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-07-25 20:42:38 -0600

    fixes asset link

--- client/src/components/dashboard/Header.jsx
+++ client/src/components/dashboard/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button';
-import logo from '../../assets/madLibsHead.png'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import logo from '../../assets/madlibsHead.png'
 const Header = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const logout = () => {
     sessionStorage.clear();
     navigate("/");
@@ -18,13 +18,24 @@ const Header = () => {
           <img src={logo} width="70px" />
         </Col>
         <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" className="p-3">
-          <Link to="/madlibs/new"><Button variant="outline-secondary" id="import-button">Import MadLib</Button></Link>
-          <Button id="logout-button" className="ms-4" variant="outline-secondary" onClick={logout}>Logout</Button>
+          <Link to="/madlibs/new">
+            <Button variant="outline-secondary" id="import-button">
+              Import MadLib
+            </Button>
+          </Link>
+          <Button
+            id="logout-button"
+            className="ms-4"
+            variant="outline-secondary"
+            onClick={logout}
+          >
+            Logout
+          </Button>
         </Col>
       </Row>
       <hr />
     </Container>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
